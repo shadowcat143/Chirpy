dded2cefd172f5ef1d519bb92b99e204e971b2be
shadowcat143 <85531159+shadowcat143@users.noreply.github.com>
2022-05-28 07:33:26 -0400

    meow

--- background.js
+++
@@ -1,6 +0,0 @@
-let color = '#808080';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
